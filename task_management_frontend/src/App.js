679f64e55b13dff1d64e5dbf781e7e0073ed85c6
Kavia AI <production@kavia.ai>
2026-01-13 04:21:22 +0000

    Checkpoint 2 - cgdac6c065

--- task_management_frontend/src/App.js
+++ task_management_frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+      <span>Hello world</span>
       <header className="App-header">
         <button 
           className="theme-toggle" 
